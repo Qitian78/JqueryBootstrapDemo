960893a6e9637811f8ab911a89de2058ec689bf7
齐恬 <1038394183@qq.com>
2018-09-28 18:09:05 +0800

    commit update

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,43 +26,43 @@ module.exports = {
     // "warn" -> 1 开启警告规则
     //"error" -> 2 开启错误规则
     rules: {
-        "eqeqeq": 1,//绝对等于
-        'arrow-parens': 0, //允许箭头函数参数使用括号
-        'generator-star-spacing': 0, //允许方法之间加星号
+        "curly": [2, "all"],//必须使用 if(){} 中的{}
+        "new-parens": 2,//new时必须加小括号
         "complexity": [2,100], //程序中允许的最大环路复杂度0
         "no-const-assign": 2, //禁止修改const声明的变量
         "no-dupe-keys": 2, //在创建对象字面量时不允许键重复 {a:1,a:1}
         "no-dupe-args": 2, //函数参数不能重复
         "no-duplicate-case": 2, //switch中的case标签不能重复
         "no-func-assign": 2,//禁止重复的函数声明
-        "no-irregular-whitespace": 1,//不能有不规则的空格
+        "no-use-before-define": 2,//未定义前不能使用
+        "no-unused-expressions": 2,//禁止无用的表达式
+        "array-bracket-spacing": [2, "never"],//是否允许非空数组里面有多余的空格
         "no-redeclare": 2,//禁止重复声明变量
         "no-regex-spaces": 2,//禁止在正则表达式字面量中使用多个空格 /foo bar/
-        "no-self-compare": 1,//不能比较自身
         "no-shadow": 2,//外部作用域中的变量不能与它所包含的作用域中的变量或参数同名
         "no-shadow-restricted-names": 2,//严格模式中规定的限制标识符不能作为声明时的变量名使用
         "no-spaced-func": 1,//函数调用时 函数名与()之间不能有空格
         "no-sparse-arrays": 1,//禁止稀疏数组， [1,,2]
         "no-trailing-spaces": 1,//一行结束后面不要有空格
         "no-undef": 1,//不能有未定义的变量
-        "one-var": 0,
         "no-implicit-coercion": 1,//禁止使用短符号进行类型转换
-        "key-spacing": 0,
-        "no-extend-native": 0,//只读对象的属性不可编辑
+        "no-loop-func": 1,//嵌套函数
+        "no-self-compare": 1,//不能比较自身
+        "no-irregular-whitespace": 1,//不能有不规则的空格
+        "eqeqeq": 1,//绝对等于
+        "no-useless-concat": 1,//无用的粘贴
+        "max-depth": [1, 5],//嵌套块深度
         "no-unneeded-ternary": 1,//禁止不必要的嵌套 var isYes = answer === 1 ? true : false;
         "no-unreachable": 1,//不能有无法执行的代码
-        "no-unused-expressions": 2,//禁止无用的表达式
-        "no-unused-vars": 0,//不能有声明后未被使用的变量或参数
-        "no-use-before-define": 2,//未定义前不能使用
+        "guard-for-in": 1,//循环而不过滤循环中的结果
         "no-var": 1,//禁用var，用let和const代替
-        "array-bracket-spacing": [2, "never"],//是否允许非空数组里面有多余的空格
         "comma-dangle": [1, "never"],//对象字面量项尾不能有逗号
         "comma-style": [1, "last"],//逗号风格，换行时在行首还是行尾
+        "no-unused-vars": 0,//不能有声明后未被使用的变量或参数
+        'arrow-parens': 0, //允许箭头函数参数使用括号
+        'generator-star-spacing': 0, //允许方法之间加星号
         "comma-spacing": 0,
-        "curly": [2, "all"],//必须使用 if(){} 中的{}
         "indent": [0, 2],//缩进风格
-        "max-depth": [1, 5],//嵌套块深度
-        "new-parens": 2,//new时必须加小括号
         "semi": [0, "always"],//语句强制分号结尾
         "strict": 0,//使用严格模式
         "quotes": 0,//字符串必须使用单引号
@@ -73,6 +73,9 @@ module.exports = {
         "object-curly-spacing": 0,
         "no-multiple-empty-lines":0,//禁止出现多行空行
         "eol-last":0,//要求或禁止文件末尾存在空行
+        "one-var": 0,//同一作用域内，一个变量只能声明一次
+        "key-spacing": 0,
+        "no-extend-native": 0,//只读对象的属性不可编辑
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
     }
 }
--- stylelint.config.js
+++ stylelint.config.js
@@ -13,8 +13,10 @@ module.exports = {
                 ignore: ['inside-function']
             }
         ],
+        'comment-whitespace-inside': null,
+        "comment-empty-line-before": null,
         // 使用数字或命名的 (可能的情况下) font-weight 值
-        'font-weight-notation': 'numeric',
+        'font-weight-notation': null,
         // 在函数的逗号之后要求有一个换行符或禁止有空白
         'function-comma-newline-after': null,
         // 在函数的括号内要求有一个换行符或禁止有空白
@@ -22,15 +24,21 @@ module.exports = {
         // url使用引号
         'function-url-quotes': 'always',
         // 禁止小于 1 的小数的前导 0
-        'number-leading-zero': 'never',
+        'number-leading-zero': 'always',
         // 字符串使用双引号
-        'string-quotes': 'double',
+        'string-quotes': 'single',
+        "selector-combinator-space-after": null,//在复合选择器之后要求或不允许留有一个空格
+        "selector-combinator-space-before": null,//在复合选择器之后要求或不允许留有一个空格
+        "declaration-colon-newline-after": null,
+        "selector-pseudo-element-colon-notation": null,
+        "font-family-no-missing-generic-family-keyword": null,
         // 要求选择器列表的逗号之前有一个换行符
-        'selector-list-comma-newline-before': 'never-multi-line',
+        'selector-list-comma-newline-before':null,
+        'selector-list-comma-newline-after':null,
         // 在媒体查询的逗号之前禁止有一换行
         'media-query-list-comma-newline-before': 'never-multi-line',
         // 缩进
-        'indentation': 4,
+        'indentation': null,
         // 禁止低优先级的选择器出现在高优先级的选择器之后
         'no-descending-specificity': null,
         // 禁止空源
